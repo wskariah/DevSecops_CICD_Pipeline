07de737e4d200522ce799b735aec8feb7db06130
Jon Fazzaro <jon@fazzaro.com>
2025-01-23 14:37:42 -0500

    test: ApprovalTests for Jenkinsfile

--- src/test/java/com/example/TheJenkinsfile.groovy
+++ src/test/java/com/example/TheJenkinsfile.groovy
@@ -1,6 +1,8 @@
 package com.example
 
 import com.lesfurets.jenkins.unit.declarative.DeclarativePipelineTest
+import org.approvaltests.Approvals
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -10,18 +12,48 @@ class TheJenkinsfile extends DeclarativePipelineTest {
     void runs() {
         runScript("Jenkinsfile")
         assertJobStatusSuccess()
+        String capturedOutput = _output.toString()
+        Approvals.verify(capturedOutput)
     }
 
     @BeforeEach
     @Override
     void setUp() {
         super.setUp()
+        arrangeGlobalProperties()
+        stubPublishHTML()
+        stubSh()
+        captureStdOutput()
+    }
+
+    @AfterEach
+    void tearDown() {
+        restoreStdOutput()
+    }
+
+    private restoreStdOutput() {
+        System.setOut(_originalOut)
+    }
+
+    private void captureStdOutput() {
+        _originalOut = System.out
+        _output = new StringWriter()
+        PrintWriter printWriter = new PrintWriter(_output)
+        System.setOut(new PrintStream(new OutputStream() {
+            @Override
+            void write(int b) throws IOException {
+                printWriter.write(b)
+            }
+        }))
+    }
+
+    StringWriter _output;
+    PrintStream _originalOut;
+
+    private void arrangeGlobalProperties() {
         binding.master = "master"
         binding.ARTIFACTORY_USERNAME = "user"
         binding.ARTIFACTORY_PASSWORD = "password"
-
-        stubPublishHTML()
-        stubSh()
     }
 
     private stubSh() {
